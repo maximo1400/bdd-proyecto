8e29c5f28ea570615b6cfcca24b621be42abe2da
Máximo Retamal <43014786+maximo1400@users.noreply.github.com>
2019-06-12 22:10:51 -0400

    46

--- App.php
+++ App.php
@@ -6,11 +6,10 @@ $port = '5412';
 $pass = 'Bot9zac';
 $charset = 'utf-8';
 
-//$dsn = "pgsql:host=$host;dbname=$db;charset=$charset";
 $dsn = "pgsql:host=$host;port=$port;dbname=$db;user=$user;password=$pass";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_COLUMN,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_LAZY,
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 try {
@@ -18,42 +17,39 @@ try {
     if($pdo)
         echo "Connected to the <strong>$db</strong> database successfully!";
 
-} catch (PDOException $e) {
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
-$stmt =$pdo->prepare("SELECT * FROM g12.worldcups;");
+$stmt =$pdo->prepare("SELECT * FROM g12.worldcups LIMIT 4;");
 $stmt->execute();
 $total_column = $stmt->columnCount();
-var_dump($total_column);
 
-for ($counter = 0; $counter < $total_column; $counter ++) {
-    $meta = $stmt->getColumnMeta($counter);
-    $column[] = $meta['name'];
+for ($j = 0; $j < $total_column; $j ++) {
+    $meta = $stmt->getColumnMeta($j);
+    $labels[] = $meta['name'];
 }
-print_r($column);
 
+echo '<table>';
+echo "<tr>";
+foreach ($labels as $key)
+    echo "<th>".$key."</th>";
+echo "</tr>";
 
-while ($row = $stmt->fetchAll(PDO::FETCH_NUM)){
-    //print_r("<p>");
-    for ($i = 0; $i <2; $i++){
-        //$row->getAttribute($i);
-        print("<pre>".print_r($row[$i],true)."</pre>");
+while ($row = $stmt->fetch()) {
+    echo "<tr>";
+        for($i=0;$i<$total_column;$i++){
+            $valor=$row[$i];
+            echo "<td>".$valor."</td>";
+        }
     }
-}
+    echo "</tr>";
+echo "<table>";
 
 
-//try {
-//    $pdo = new PDO('pgsql:host=cc3201.dcc.uchile.cl;port=5412;dbname=cc3201;user=cc3201;password=Zac8bot');
-//    echo "PDO connection object created";
-//    $stmt = $pdo->query('SELECT * FROM worldcups');
-//    while ($row = $stmt->fetch())
-//    {
-//        print_r($row);
-//    }
-//}
-//catch(PDOException $e)
-//{
-//    echo $e->getMessage();
-//}
+
+
+
+
+
 
 ?>
---
+++ loQueQuedoComentadoYNoElimine.php
@@ -0,0 +1,89 @@
+<?php
+//print("<pre>".print_r($column,true)."</pre>");
+//print_r($column);
+
+//$row = $stmt->fetch();
+//print("<pre>".print_r($row,true)."</pre>");
+
+//foreach ($row as $trash)
+//    foreach ($trash as $data){
+//        $valor=$data;
+//        echo "<td>".$valor."</td>";
+//    }
+//for($i=0;$i<$total_column;$i++)
+//    $valor=$row[$i];
+//echo "<td>".$valor."</td>";
+
+//    echo "<td>".$row[0]."</td>";
+//    echo "<td>".$row[1]."</td>";
+//    echo "<td>".$row[2]."</td>";
+//    echo "<td>".$row[3]."</td>";
+//    echo "<td>".$row[4]."</td>";
+//    echo "<td>".$row[5]."</td>";
+//    echo "<td>".$row[6]."</td>";
+//    echo "<td>".$row[7]."</td>";
+//    echo "<td>".$row[8]."</td>";
+//    echo "<td>".$row[9]."</td>";
+
+
+//$row = $stmt->fetch();
+//print("<pre>".print_r($row,true)."</pre>");
+//
+//$row = $stmt->fetch();
+//$row = $stmt->fetch();
+//$row = $stmt->fetch();
+//print($row);
+//print("<pre>".print_r($row,true)."</pre>");
+
+
+//
+//try {
+//    for ($counter = 0; $counter < 1; $counter ++) {
+//        $row = $stmt->fetch();
+//        print("<pre>".print_r($row,true)."</pre>");
+//    }
+//} catch (Exception $e) {
+//    print("error");
+//    //print($e->getMessage());
+//    // throw new PDOException($e->getMessage(), (int)$e->getCode());
+//}
+
+//$row = $stmt->fetch();
+//print($row[0]);
+//
+//$row = $stmt->fetch();
+//print($row[0]);
+
+
+
+
+
+
+//foreach ($conn->query($sql) as $row) {
+//    print $row['name'] . "\t";
+//    print $row['color'] . "\t";
+//    print $row['calories'] . "\n";
+//}
+
+
+//try {
+//    $pdo = new PDO('pgsql:host=cc3201.dcc.uchile.cl;port=5412;dbname=cc3201;user=cc3201;password=Zac8bot');
+//    echo "PDO connection object created";
+//    $stmt = $pdo->query('SELECT * FROM worldcups');
+//    while ($row = $stmt->fetch())
+//    {
+//        print_r($row);
+//    }
+//}
+//catch(PDOException $e)
+//{
+//    echo $e->getMessage();
+//}
+
+//while ($row = $stmt->fetchAll(PDO::FETCH_NUM)){
+//    for ($i = 0; $i <1; $i++){
+//        //$row->getAttribute($i);
+//        print("<pre>".print_r($row[$i],true)."</pre>");
+//    }
+//}
+?>
